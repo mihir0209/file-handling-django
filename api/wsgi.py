ca808fa0b82f8c9db27b3eea13b463ae6ec5d1ae
Mihir Patil <160046567+mihir0209@users.noreply.github.com>
2024-10-15 15:23:03 +0530

    Update wsgi.py

--- api/wsgi.py
+++ api/wsgi.py
@@ -6,11 +6,15 @@ It exposes the WSGI callable as a module-level variable named ``app``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
-
 import os
-
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
-app = get_wsgi_application()
+application = get_wsgi_application()
+
+if __name__ == "__main__":
+    from wsgiref.simple_server import make_server
+    port = int(os.getenv('PORT', 8000))  # Default to 8000 if PORT is not set
+    httpd = make_server('0.0.0.0', port, application)
+    httpd.serve_forever()
