f305ac62e1a7fec7b09a767896f6bb4b94067ddb
Mihir Patil <160046567+mihir0209@users.noreply.github.com>
2024-10-16 00:41:52 +0530

    Update settings.py

--- api/settings.py
+++ api/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-=cldztbc4jg&xl0!x673!*v2_=p$$eu)=7*f#d0#zs$44xx-h^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'api.wsgi.railway.app', 'file-handling-django-production.up.railway.app','file-handling-django.onrender.com']
-CSRF_TRUSTED_ORIGINS = ['https://api.wsgi.railway.app', 'https://file-handling-django-production.up.railway.app', 'https://pdfhandlerpro.onrender.com','https://file-handling-django.onrender.com']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', 'file-handling-django-production.up.railway.app','file-handling-django.onrender.com']
+CSRF_TRUSTED_ORIGINS = ['https://file-handling-django-production.up.railway.app', 'https://pdfhandlerpro.onrender.com','https://file-handling-django.onrender.com']
 
 # Media files
 if os.environ.get('READ_ONLY_FILE_SYSTEM'):  # Custom environment variable to detect serverless
